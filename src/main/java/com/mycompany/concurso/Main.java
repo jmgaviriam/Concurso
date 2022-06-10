7a64080ba67acc27cda70bfcaa4668d2bdbebf6b
Jhoan Gaviria <jmgaviriam@gmail.com>
2022-06-09 19:58:41 -0500

    initialization

---
+++ src/main/java/com/mycompany/concurso/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.concurso;
+
+/**
+ *
+ * @author Jhoan Gaviria
+ */
+public class Main {
+    
+}
